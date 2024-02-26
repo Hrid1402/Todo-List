48bb4d564949e42ec1ffb8cc8a9849a1587eea67
Hrid <Hrid1402@gmail.com>
2024-02-25 22:41:13 -0500

    Almost done with task editing

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ console.log("working");
 
 const addBTN = document.querySelector("#addBTN");
 const dialog = document.querySelector("#dialog");
+const descriptionDialog = document.querySelector("#description_Dialog");
 const closeDialogBTN = document.querySelector("#closeDialog");
 const createBTN = document.querySelector("#createBTN");
 
 const titleValue = document.querySelector("#title");
-const descriptionValue = document.querySelector("#description");
 const dateValue = document.querySelector("#date");
 
 const lowBTN = document.querySelector("#low");
@@ -19,6 +19,19 @@ const allTasks = document.querySelector("#AllTasks");
 let taskList = [];
 let globalBottomID = 0;
 
+//DESCRIIPTION PART
+const descriptionValue = document.querySelector("#description");
+const closeBTN = document.querySelector("#closeBTN");
+const d_Title = document.querySelector("#d_Title");
+const d_description = document.querySelector("#description_D");
+const d_priority = document.querySelector("#description");
+const d_date = document.querySelector("#date_D");
+const D_lowBTN = document.querySelector("#low_D");
+const D_mediumBTN = document.querySelector("#medium_D");
+const D_highBTN = document.querySelector("#high_D");
+
+
+//ADD
 addBTN.addEventListener("click",()=>{
     console.log("Add Clicked");
     enablePriorityButtons();
@@ -30,7 +43,7 @@ addBTN.addEventListener("click",()=>{
     dateValue.value = todayInFormat;
     dialog.showModal();
 });
-
+//CREATE
 createBTN.addEventListener("click",()=>{
     console.log("Created!");
 
@@ -54,13 +67,22 @@ createBTN.addEventListener("click",()=>{
     console.log(taskList);
     dialog.close();
 });
+closeBTN.addEventListener("click",()=>{
+    
+    descriptionDialog.close();
+});
 
 function enablePriorityButtons(){
+    //MainOnes
     lowBTN.disabled = false;
     mediumBTN.disabled = false;
     highBTN.disabled = false;
+    //Others
+    D_lowBTN.disabled = false;
+    D_mediumBTN.disabled = false;
+    D_highBTN.disabled = false;
 };
-
+//Main Priority buttons
 lowBTN.addEventListener("click",()=>{
     enablePriorityButtons();
     lowBTN.disabled = true;
@@ -75,12 +97,35 @@ highBTN.addEventListener("click",()=>{
     enablePriorityButtons();
     highBTN.disabled = true;
 });
+//Description Priority Buttons
 
+D_lowBTN.addEventListener("click",()=>{
+    enablePriorityButtons();
+    D_lowBTN.disabled = true;
+});
+
+D_mediumBTN.addEventListener("click",()=>{
+    enablePriorityButtons();
+    D_mediumBTN.disabled = true;
+});
+
+D_highBTN.addEventListener("click",()=>{
+    enablePriorityButtons();
+    D_highBTN.disabled = true;
+});
+//CLOSE MAIN DIALOG
 closeDialogBTN.addEventListener("click",()=>{
     dialog.close();
 });
-
-
+//GET INDEX
+function getIndexofElement(element){
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id == element) {
+            return taskList.indexOf(taskList[i]);
+        }
+    }
+}
+//DOOM
 function createTaskElements(titleValue, descriptionValue, dateValue, priorityValue, idValue){
     console.log("created!");
     const taskBlock = document.createElement("div");
@@ -107,12 +152,15 @@ function createTaskElements(titleValue, descriptionValue, dateValue, priorityVal
     priority.classList.add("priority");
     if(priorityValue == "low"){
         //green
+        D_lowBTN.disabled = true;
         priority.style.backgroundColor = "#7ED957";
     }else if(priorityValue == "medium"){
         //yellow
+        D_mediumBTN.disabled = true;
         priority.style.backgroundColor = "#FEC95F";
     }else if(priorityValue == "high"){
         //red
+        D_highBTN.disabled = true;
         priority.style.backgroundColor = "#FF5757";
     }
 
@@ -133,17 +181,14 @@ function createTaskElements(titleValue, descriptionValue, dateValue, priorityVal
     trashIMG.addEventListener("click",(eventC)=>{
         eventC.stopPropagation();
         const DOMtasks = document.querySelectorAll(".taskBlock");
-        taskList.forEach(task => {
-            if(task.id == idValue){
-                let index = taskList.indexOf(task);
-                console.log("index", index);
-                taskList.splice(index, 1);
-                while (DOMtasks[index].firstChild) {
-                    DOMtasks[index].removeChild(DOMtasks[index].firstChild);
-                }
-                DOMtasks[index].remove();
-            }
-        });
+        //getIndex
+        const index = getIndexofElement(idValue);
+        console.log("index", index);
+        taskList.splice(index, 1);
+        while (DOMtasks[index].firstChild) {
+            DOMtasks[index].removeChild(DOMtasks[index].firstChild);
+        }
+        DOMtasks[index].remove();
     });
     
     trashIMG.classList.add("Trash");
@@ -151,9 +196,18 @@ function createTaskElements(titleValue, descriptionValue, dateValue, priorityVal
     end.appendChild(trashIMG);
     end.classList.add("end");
     taskBlock.appendChild(end);
+    //CLICK THE BLOCK
     taskBlock.addEventListener("click",()=>{
         console.log("taskBlockClicked");
+        descriptionDialog.showModal();
+        console.log("Tile: " + titleValue);
+        d_Title.textContent = titleValue;
+        d_description.textContent = descriptionValue;
+        console.log(dateValue);
+        d_date.value = dateValue;
+        console.log(priorityValue);
     });
+
     allTasks.appendChild(taskBlock);
 }
 
